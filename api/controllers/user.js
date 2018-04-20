cedff16b8244e4454865a8b1de0df77466e7c959
Justin Kaseman <justinkaseman@live.com>
2018-04-20 09:22:26 -0700

    Finish User Controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,10 +3,14 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
-  // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  if (!(username && password))
+    res.status(422).json('provide name and password, yo');
+  else
+    User.create({ username: username, password: password })
+      .then(user => res.status(201).json('created user'))
+      .catch(error => res.status(500).json(error));
 };
 
 module.exports = {
-  createUser
+  createUser,
 };
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -14,11 +14,11 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
-  bcrypt.hash(this.password, 12, function(err, hash) {
-    if (err) next(err);
-    else this.password = hash;
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    return next();
   });
-  next();
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -26,9 +26,7 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-  UserSchema.find({})
-    .then(user => bcrypt.compare(plainTextPW, user.password, callBack))
-    .catch(error => res.status(402).json(error));
+  return bcrypt.compare(plainTextPW, this.password, callBack);
 };
 
 module.exports = mongoose.model('User', UserSchema);
